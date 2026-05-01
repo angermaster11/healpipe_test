0d03c92839301190808f23c03c9af2ad219f71fc
Arju Srivastava <krishnabhakt677@gmail.com>
2026-05-02 04:19:27 +0530

    test

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ def test_normal_discount():
 
     response = client.get("/calculate/discount?price=1000&discount_percentage=20")
     assert response.status_code == 200
-    assert response.json() == {"final_price": 800.0}
+    assert response.json() == {"final_price": 800.00}
 
 def test_invalid_discount():
 
