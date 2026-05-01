964158ef91c9a7cad703756c82b64a7b7d3216bc
Arju Srivastava <krishnabhakt677@gmail.com>
2026-05-02 02:28:44 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn  
+
+app = FastAPI()
+
+@app.get("/calculate/discount")
+def calculate_discount(price: float, discount_percentage: float):
+
+    
+    final_price = price - (price * (discount_percentage / 100))
+    return {"final_price": final_price}
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Discount Calculator API!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0", port=8001)
---
+++ test.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_normal_discount():
+
+    response = client.get("/calculate/discount?price=1000&discount_percentage=20")
+    assert response.status_code == 200
+    assert response.json() == {"final_price": 800.0}
+
+def test_invalid_discount():
+
+    response = client.get("/calculate/discount?price=1000&discount_percentage=-10")
+    assert response.status_code == 400
